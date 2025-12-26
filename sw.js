a4a02a0e11c4098666e47b123a23b7cff09881dc
adiyuashva-beep <adiyu.ashva@gmail.com>
2025-12-26 12:55:47 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
 self.addEventListener('install', (e) => {
   console.log('[SiGanteng] Install Service Worker...');
   e.waitUntil(
-    caches.open('siganteng-store').then((cache) => cache.addAll([
+    // 1. GANTI NAMA CACHE JADI v2 (Biar browser download ulang)
+    caches.open('siganteng-store-v2').then((cache) => cache.addAll([
       './',
       './index.html',
       './siswa.html',
       './guru.html',
+      './manifest.json', // Tambahin ini
+      './logo.png',      // Tambahin ini
+      './splash.png',    // WAJIB Tambahin ini biar splash screen kesimpen
     ]))
   );
 });
@@ -14,5 +18,19 @@ self.addEventListener('fetch', (e) => {
   console.log('[SiGanteng] Fetching url: ' + e.request.url);
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
+
+// TAMBAHAN: Hapus Cache Lama (v1) biar memori HP gak penuh
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== 'siganteng-store-v2') {
+          console.log('[SiGanteng] Hapus cache lama', key);
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
